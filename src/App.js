2a589984a74bf85f6556cb48e9e90ff6ddc11d62
Michael <123pie23@gmail.com>
2019-12-26 23:27:16 -0800

    started the ;ayout on the stories page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header';
 import Signin from './components/logins/signin'
 import Signup from './components/logins/signup';
 import Profile from './components/profile';
+import Stories from './components/stories'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
@@ -12,11 +13,12 @@ class App extends Component {
         return (
             <Router>
                 <div className="App">
-                    <Route path={"*"} component={Header}/>
+                    {/*<Route path={"*"} component={Header}/>*/}
                     <Route exact path={"/"} component={Landing}/>
                     <Route path={"/profile"} component={Profile} />
                     <Route path={"/signin"} component={Signin} />
                     <Route path={"/signup"} component={Signup} />
+                    <Route path={'/stories'} component={Stories}></Route>
                 </div>
             </Router>
 
---
+++ src/components/stories/index.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import AuthHOC from '../../HOC/authHOC';
+
+class Stories extends Component {
+
+    state = {
+
+    };
+
+    render() {
+        return (
+            <div className="stories-container">
+                <div className="stories-gutter">
+                    <div className="stories-header-container">
+                        <div className="profile-info">
+                            <div className="stories-profile-img-container">
+                                <img className="stories-profile-img" src="" alt="instagram story profile"/>
+                            </div>
+                            <div className="stories-username">usernamehereeeee</div>
+                            <div className="stories-posted-at">1h</div>
+                        </div>
+                        <div className="stories-paused">Paused</div>
+                        <div className="stories-option">
+                            <img className="stories-ellipsis" src={this.props.generalImages['ellips.png']} alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps, {
+
+})(AuthHOC(Stories))
