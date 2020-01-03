6eeff01c6b6ce193f62f533f695a1da47e843411
Michael <123pie23@gmail.com>
2020-01-02 23:23:32 -0800

    fixed the getters for the stories to not get viewed stories and to not get user information if all of their stories are seen

--- backend/getStoriesProfile.js
+++ backend/getStoriesProfile.js
@@ -4,71 +4,58 @@ module.exports = (app, db) => {
         let output = {status: 'NO'};
         let followIDs = [];
         let userStories = [];
-
+        let userStoriesIndex = 0;
+        //get all the accounts this account follows
         let sql = "SELECT `followAccount` FROM `followers` WHERE `accountID` = ?";
         db.connect(() => {
             db.query(sql, id, (err, followingListData) => {
-                if(!err) {
-                    for(let followingIndex in followingListData) {
-                        let currentFollowID = followingListData[followingIndex].followAccount;
-                        followIDs.push(currentFollowID)
-                    }
-                    for (let followerID in followIDs) {
-                        let sql2 = "SELECT `m`.`fileName`, `m`.`created_at`, `a`.`username`, `a`.`name`, `a`.`ID`, (SELECT `created_at` FROM `media` WHERE `accountID` = ? AND `mediaType` = 'story' ORDER BY `created_at` DESC LIMIT 1) AS `storyCreatedAt` FROM `media` AS m JOIN `accounts` AS a ON (`m`.`accountID` = `a`.`ID`) WHERE `accountID` = '2' AND `m`.`mediaType` = 'profile' ORDER BY `m`.`created_at`";
-                        let followID = followIDs[followerID];
-                        db.query(sql2, [followID, followID], (err, data) => {
-                            if(err) throw err;
-                            userStories[followerID] = data;
-                            if(followerID == followIDs.length -1) {
-                                output = {
-                                    status: 'OK',
-                                    stories: userStories,
-                                };
-                                res.send(output)
-
-                            }
-                        })
-                    }
-                } else {
-                    console.log('Query Error (1) --get stories service');
+                if(err) throw err;
+                for(let followingIndex in followingListData) {
+                    let currentFollowID = followingListData[followingIndex].followAccount;
+                    followIDs.push(currentFollowID)
+                }
+                for (let followerID in followIDs) {
+                    //get all the media from the current account
+                    let sql2 = "SELECT `ID` FROM `media` WHERE `accountID` = ? AND `mediaType` = 'story'";
+                    let followID = followIDs[followerID];
+                    db.query(sql2, followID, (err, allUserStories) => {
+                        let viewedCount = 0;
+                        for(let story in allUserStories) {
+                            let sql3 = "SELECT COUNT(*) AS `viewed_story` FROM `viewed_stories` WHERE `accountID` = ? AND `mediaID` = ?";
+                            let currentStory = allUserStories[story].ID;
+                            db.query(sql3, [id, currentStory], (err, viewed_story_data) => {
+                                if(err) throw err;
+                                if(viewed_story_data[0].viewed_story != 0) {
+                                    viewedCount++;
+                                }
+                                if((story == (allUserStories.length - 1)) && (viewedCount != allUserStories.length)) {
+                                    let sql4 = "SELECT `m`.`ID` AS `mediaID`, `m`.`fileName`, `m`.`created_at`, `a`.`username`, `a`.`name`, `a`.`ID`, (SELECT `created_at` FROM `media` WHERE `accountID` = ? AND `mediaType` = 'story' ORDER BY `created_at` DESC LIMIT 1) AS `storyCreatedAt` FROM `media` AS m JOIN `accounts` AS a ON (`m`.`accountID` = `a`.`ID`) WHERE `accountID` = ? AND `m`.`mediaType` = 'profile' ORDER BY `m`.`created_at`";
+                                    db.query(sql4, [followID, followID], (err, data) => {
+                                        userStories[userStoriesIndex] = data;
+                                        userStoriesIndex++;
+                                        if(followerID == (followIDs.length -1)) {
+                                            output = {
+                                                status: 'OK',
+                                                stories: userStories,
+                                            };
+                                            console.log('userStories58: '+JSON.stringify(userStories, null, 2));
+                                            res.send(output);
+                                        }
+                                    })
+                                } else {
+                                    if(followerID == (followIDs.length - 1)) {
+                                        output = {
+                                            status: 'OK',
+                                            stories: userStories,
+                                        };
+                                        res.send(output)
+                                    }
+                                }
+                            })
+                        }
+                    })
                 }
             });
         })
     })
 };
-
-// module.exports = (app, db) => {
-//     app.post('/getStories', (req, res) => {
-//         let {id} = req.headers;
-//         let output = {status: 'NO'};
-//         let userStories = [];
-//
-//         let sql = "SELECT `followAccount` FROM `followers` WHERE `accountID` = ?";
-//         db.connect(() => {
-//             db.query(sql, id, (err, followingListData) => {
-//                 if(!err) {
-//                     for(let followingIndex in followingListData) {
-//                         let currentFollowID = followingListData[followingIndex].followAccount;
-//                         let sql2 = "SELECT `m`.`fileName`, `m`.`created_at`, `a`.`username`, `a`.`name`, `a`.`ID` FROM `media` AS m JOIN `accounts` AS a ON (`m`.`accountID` = `a`.`ID`) WHERE `accountID` = ? AND `m`.`mediaType` = 'profile' ORDER BY `m`.`created_at`";
-//                         db.query(sql2, [currentFollowID], (err, data) => {
-//                             if(err) throw err;
-//                             userStories[followerID] = data;
-//                             if(Number(followingIndex) == followingListData.length -1) {
-//                                 output = {
-//                                     status: 'OK',
-//                                     stories: userStories,
-//                                 };
-//                                 res.send(output)
-//
-//                             }
-//                         })
-//
-//                     }
-//                 } else {
-//                     console.log('Query Error (1) --get stories service');
-//                 }
-//             });
-//         })
-//     })
-// };
-//
