9312d189a04ad1fab24e28c67244098cd2c8d12f
Michael <123pie23@gmail.com>
2020-01-06 22:24:22 -0800

    removed commented code

--- src/components/postModal/index.js
+++ src/components/postModal/index.js
@@ -55,34 +55,6 @@ class PostModal extends Component {
                         </div>
                     </div>
                 </div>
-                {/*<div className="post-modal-content-container">*/}
-                    {/*<div className="post-modal-content">*/}
-                        {/*<div className="post-modal-img-container">*/}
-                            {/*<img className="post-modal-img" src={this.props.generalImages['default.png']} alt=""/>*/}
-                        {/*</div>*/}
-                        {/*<div className="post-modal-content-left-container">*/}
-                            {/*<div className="post-modal-content-header">*/}
-                                {/*<div className="post-modal-left-img-container">*/}
-                                    {/*<img className="post-modal-left-img" src={this.props.generalImages['default.png']} alt=""/>*/}
-                                {/*</div>*/}
-                                {/*<div className="post-modal-content-left-top-container">*/}
-                                    {/*<div className="post-modal-content-left-username">uesgfughseuifgsue</div>*/}
-                                    {/*<div className="post-modal-content-left-dot">•</div>*/}
-                                    {/*<div className="post-modal-content-left-follow">Follow</div>*/}
-                                {/*</div>*/}
-                                {/*<div className="post-modal-content-left-ellipsis-container">*/}
-                                    {/*<img className="post-modal-content-left-ellipsis-img" src={this.props.generalImages['ellipsis.png']} alt=""/>*/}
-                                {/*</div>*/}
-                            {/*</div>*/}
-                            {/*<div className="post-modal-comments-container"></div>*/}
-                            {/*<div className="post-modal-likes-media-container"></div>*/}
-                            {/*<div className="post-modal-add-comment">*/}
-                                {/*<input ref={this.myInput} onChange={() => this.myInput.current.value !== '' ? this.state.enableBtn = true : this.state.enableBtn = false} placeholder="Add a comment..." className="add-comment-input" type="text" maxLength="140" name="comment"/>*/}
-                                {/*<button onClick={() => {this.createComment(this.myInput.current)}} disabled={this.state.enableBtn} className="post-media-add-comment-btn btn">Post</button>*/}
-                            {/*</div>*/}
-                        {/*</div>*/}
-                    {/*</div>*/}
-                {/*</div>*/}
             </div>
         )
     }
