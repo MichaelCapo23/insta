169dd13d18434490737bf7b60d995fc62747f6e1
Michael <123pie23@gmail.com>
2020-02-10 23:30:46 -0800

    worked on the upload and select modal

--- backend/getUsername.js
+++ backend/getUsername.js
@@ -9,57 +9,69 @@ module.exports = (app, db) => {
                     res.sendStatus(500);
                     return;
                 }
+                let id = data[0].ID;
                 if (data.length > 0) {
-                    if(username !== 'undefined') {
-                        let sql = "SELECT `ID`, `name`, `username`, IFNULL(( SELECT `bio` FROM `accounts` WHERE `username` = ?), '') AS bio FROM `accounts` WHERE `username` = ?";
-                        db.query(sql, [username, username], (err, follower) => {
-                            if (err) {
-                                console.log(err);
-                                res.sendStatus(500);
-                                return;
-                            }
-                            let id = data[0].ID;
-                            let followerid = follower[0].ID;
+                    let sql = "SELECT IFNULL((SELECT `fileName` FROM `media` WHERE `accountID` = ? AND `mediaType` = 'profile'), 'default') AS `profileFileName`";
+                    db.query(sql, id, (err, fileNameData) => {
+                        if (err) {
+                            console.log(err);
+                            res.sendStatus(500);
+                            return;
+                        }
+                        let profileFileName = fileNameData[0].profileFileName;
 
-                            let sql = "SELECT COUNT(*) AS `count` FROM `followers` WHERE `accountID` = ? AND `followAccount` = ?";
-                            db.query(sql, [id, followerid], (err, followsUserData) => {
+                        if (username !== 'undefined') {
+                            let sql = "SELECT `ID`, `name`, `username`, IFNULL(( SELECT `bio` FROM `accounts` WHERE `username` = ?), '') AS bio FROM `accounts` WHERE `username` = ?";
+                            db.query(sql, [username, username], (err, follower) => {
                                 if (err) {
                                     console.log(err);
                                     res.sendStatus(500);
                                     return;
                                 }
+                                let followerid = follower[0].ID;
 
-                                if (follower.length > 0) {
-                                    let output = {
-                                        status: "OK",
-                                        username: data[0].username,
-                                        id: id,
-                                        name: data[0].name,
-                                        bio: data[0].bio,
-                                        followerusername: follower[0].username,
-                                        followerid: followerid,
-                                        followername: follower[0].name,
-                                        followerbio: follower[0].bio,
-                                        followsUser: followsUserData[0].count,
-                                    };
-                                    res.send(output)
-                                }
+                                let sql = "SELECT COUNT(*) AS `count` FROM `followers` WHERE `accountID` = ? AND `followAccount` = ?";
+                                db.query(sql, [id, followerid], (err, followsUserData) => {
+                                    if (err) {
+                                        console.log(err);
+                                        res.sendStatus(500);
+                                        return;
+                                    }
+
+                                    if (follower.length > 0) {
+                                        let output = {
+                                            status: "OK",
+                                            username: data[0].username,
+                                            id: id,
+                                            name: data[0].name,
+                                            bio: data[0].bio,
+                                            profileFileName: profileFileName,
+                                            followerusername: follower[0].username,
+                                            followerid: followerid,
+                                            followername: follower[0].name,
+                                            followerbio: follower[0].bio,
+                                            followsUser: followsUserData[0].count,
+                                        };
+                                        res.send(output)
+                                    }
+                                })
                             })
-                        })
-                    } else {
-                        let output = {
-                            status: "OK",
-                            username: data[0].username,
-                            id: data[0].ID,
-                            name: data[0].name,
-                            bio: data[0].bio,
-                            followerusername: '',
-                            followerid: '',
-                            followername: '',
-                            followerbio: 'none',
-                        };
-                        res.send(output)
-                    }
+                        } else {
+                            let output = {
+                                status: "OK",
+                                username: data[0].username,
+                                id: data[0].ID,
+                                name: data[0].name,
+                                bio: data[0].bio,
+                                profileFileName: profileFileName,
+                                followerusername: '',
+                                followerid: '',
+                                followername: '',
+                                followerbio: 'none',
+                            };
+                            res.send(output)
+                        }
+                    })
                 }
             })
         })
--- src/components/profile/fineUploader.js
+++ src/components/profile/fineUploader.js
@@ -1,19 +1,42 @@
-import React, {Component} from 'react'
+import React, {Component, useRef} from 'react'
 import ReactFileReader from 'react-file-reader';
+import Select from 'react-select';
 
 class UploadComponent extends Component {
 
+    state = {
+        selectedOption: [],
+    };
+
+    handleChange = selectedOption => {
+        this.setState({
+            selectedOption: selectedOption,
+        });
+        console.log(`Option selected:`, selectedOption);
+    };
+
     render() {
+        const { selectedOption } = this.state;
+        let options = [
+            { value: '1', label: 'mike' },
+            { value: '2', label: 'shawn' },
+            { value: '3', label: 'omer' },
+        ];
         return(
             <div className="files">
                 <ReactFileReader base64={true} multipleFiles={true} multiple="multiple" handleFiles={this.props.handleFilesFns}>
                     <button className='btn'>Upload</button>
                 </ReactFileReader>
-                <img className="preview-upload-img" src={this.props.state.file === '' ? '' : this.props.state.file} frameBorder="0" height="400" width="50%" />
+                {this.props.state.file === '' ? <div className="no-img-selected-text">No Image Has Been Selected!</div> : <img className="preview-upload-img" src={this.props.state.file} frameBorder="0" height="400" width="50%" />}
+                <div className={this.props.state.file == '' ? 'error-text' : 'hide'}></div>
                 <div className="info-container">
                     <div className="settings-profile-content email-input">
-                        <div className="create-post-field-name">description</div>
-                        <input onChange={this.props.addMediaDescFns} id="post-desc" className="create-post-field-input"/>
+                        <div className="create-post-field-name">Description: </div>
+                        <textarea onChange={this.props.addMediaDescFns} id="post-desc" className="create-post-field-input"/>
+                        <div className="select-container">
+                            <label className="select-label" htmlFor="selectID">Tag Friends: </label>
+                            <Select id="selectID" isMulti value={selectedOption} onChange={this.handleChange} options={options}/>
+                        </div>
                     </div>
                 </div>
             </div>
