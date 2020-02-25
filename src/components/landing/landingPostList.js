2543efc59197a8511f50a6f8ce83bd2ff96a8a6a
Michael <123pie23@gmail.com>
2020-02-24 23:06:21 -0800

    worked on the comments and tags logic

--- src/components/landing/landingPostList.js
+++ src/components/landing/landingPostList.js
@@ -1,14 +1,15 @@
 import React, {useState, useRef} from 'react';
 export default props => {
     let [disabledVal, enableBtn] = useState(true);
-    let {mediaID, comments, likes, fileName, posterID, lastLikedFileName, lastLikedUsername, posterFileName, posterUsername, userLiked, userSaved} = props.media;
+    let [showTags, tagStatusChange] = useState(false);
+    let {mediaID, comments, likes, fileName, posterID, lastLikedFileName, lastLikedUsername, posterFileName, posterUsername, userLiked, userSaved, tags} = props.media;
     let {profileImages, generalImages, mediaImages} = props.images;
 
     function newCommentsArr(comments) {
         let arr = [];
         arr.push(comments[0]);
-        arr.push(comments[1]);
         arr.push('');
+        arr.push(comments[1]);
         arr.push(comments[comments.length-1]);
         return arr;
     }
@@ -16,6 +17,7 @@ export default props => {
     const myInput = useRef();
     const likeBtn = useRef();
     const saveBtn = useRef();
+    const ellipsis = useRef();
 
 
     const createComment = (input) => {
@@ -25,15 +27,15 @@ export default props => {
 
     const likeMedia = (likeBtn) => {
         props.likeFunction(likeBtn, posterID);
-        // likeBtn.setAttribute('src',generalImages['heartRed.png']);
     };
 
-    const openModalFns = () => {
-        let imgTag = document.getElementById("ellipsis-img");
-        let posterID = imgTag.attributes['data-posterid'];
-        let username = imgTag.attributes['data-username'];
-        let filename = imgTag.attributes['data-filename'];
-        props.openModal({posterID: posterID, username:username, filename:filename});
+    const openModalFns = (e) => {
+        let posterID = ellipsis.current.attributes['data-posterid'];
+        let username = ellipsis.current.attributes['data-username'];
+        let filename = ellipsis.current.attributes['data-filename'];
+        let mediaID = ellipsis.current.attributes['data-mediaid'];
+        let optionsVal = e.currentTarget.attributes['data-optionsval'];
+        props.openModal({posterID: posterID, username:username, filename:filename, mediaID:mediaID, optionsVal:optionsVal});
     };
 
     function saveMedia() {
@@ -44,33 +46,43 @@ export default props => {
     function callCommentIcon() {
         myInput.current.focus();
         myInput.current.select();
-    };
+    }
 
     return (
         <div className="landing-media-container">
             <div className="landing-media-header">
                 <div className="poster-profile-pic">
-                    <img className='poster-profile-pic-img' src={profileImages[posterFileName]} alt="instagram poster profile picture"/>
+                    <img className='poster-profile-pic-img' src={!posterFileName || posterFileName == 'default' ? generalImages['default.png'] : profileImages[posterFileName]} alt="instagram poster profile picture"/>
                 </div>
                 <div className="poster-username-container">{posterUsername}</div>
                 <div className="ellipsis">
-                    <img data-posterid={posterID} data-username={posterUsername} data-filename={posterFileName} onClick={(e) => openModalFns(e)} id='ellipsis-img' className="ellipsis-img" src={generalImages['ellipsis.png']} alt=""/>
+                    <img ref={ellipsis} data-optionsval="menu" data-posterid={posterID} data-filename={posterFileName ? posterFileName : 'default'} data-username={posterUsername} data-mediaid={mediaID} onClick={(e) => openModalFns(e)} id='ellipsis-img' className="ellipsis-img" src={generalImages['ellipsis.png']} alt=""/>
                 </div>
             </div>
 
             <div className="landing-media-pic">
                 <img className="landing-media-img" src={mediaImages[fileName]} alt=""/>
+                <div onClick={() => tagStatusChange(showTags === true ? false : true)} className={tags.length > 0 ? 'taggedButton' : 'hide'}>
+                    <span className="taggedImg"/>
+                </div>
+                <div className={showTags ? 'tagContainer' : 'hide'}>
+                    {tags.map((item, index) => (
+                        <div className={`tagDivPosition${index} tagDiv`}>
+                            <div  key={index} className="innerTagDiv">{item.taggedUsername}</div>
+                            <div key={index} className="tag-point"></div>
+                        </div>
+                    ))}
+                </div>
             </div>
 
             <div className="landing-media-footer">
                 <div className="material-icons-container">
                     <div className="heart-container">
-                        <img onClick={(e) => likeMedia(likeBtn.current)} data-media={comments[0].mediaID} ref={likeBtn} className="heart-img-landing" src={userLiked === 1 ? generalImages['heartRed.png'] : generalImages['heartClear.jpg']} alt=""/>{/*generalImages['heartRed.png']*/}
+                        <img onClick={(e) => likeMedia(likeBtn.current)} data-media={comments ? comments.mediaID : ''} ref={likeBtn} className="heart-img-landing" src={userLiked === 1 ? generalImages['heartRed.png'] : generalImages['heartClear.jpg']} alt=""/>{/*generalImages['heartRed.png']*/}
                     </div>
                     <div className="comment-container">
                         <img onClick={callCommentIcon} className="comment-img-landing" src={generalImages['comment.png']} alt=""/>
                     </div>
-                    {/*<div className="material-icons like-comment landing-comments">mode_comment</div>*/}
                     <div ref={saveBtn} onClick={saveMedia} className="material-icons landing-bookmark">{userSaved == '0' ? 'bookmark_border' : 'bookmark'}</div> {/* bookmark <-- this is for when they click it, change to this to show media has been saved*/}
                 </div>
                 <div className="liked-container">
@@ -86,10 +98,10 @@ export default props => {
                     {comments.length < 4 ? comments.map((item, index) => (
                         <div key={'landing-inner1'+index} className="comment-container-landing-inner"><span className="commenter-name">{item.commenter}</span><span className="comment-text">{item.comment}</span><span key={index} className="material-icons like-comment-landing">thumb_up</span></div>
                     )) : threeComments.map((item, index) => (
-                        (index === 0 || index === 1 || index === 3) ? <div key={'landing-inner'+index} className="comment-container-landing-inner"><span className="commenter-name">{item.commenter}</span><span className="comment-text">{item.comment}</span><span key={index} className="material-icons like-comment-landing">thumb_up</span></div> : <div key={index} className="comment-container-landing-inner"><span className="view-comments-span">View all {comments.length - 3} comments</span></div>
+                        (index === 0 || index === 2 || index === 3) ? <div key={'landing-inner'+index} className="comment-container-landing-inner"><span className="commenter-name">{item.commenter}</span><span className="comment-text">{item.comment}</span><span key={index} className="material-icons like-comment-landing">thumb_up</span></div> : <div onClick={(e) => openModalFns(e)}  data-optionsval="comments" key={index} className="comment-container-landing-inner"><span className="view-comments-span">View all {comments.length - 3} comments</span></div>
                     ))}
                 </div>
-                <div className="days-posted-container"></div>
+                <div className="days-posted-container"/>
             </div>
 
             <div className="landing-media-add-comment">
--- src/components/landing/postOptionsModal.js
+++ src/components/landing/postOptionsModal.js
@@ -13,6 +13,11 @@ class OptionsModal extends Component {
         document.getElementById("optionsModal").classList.remove("hide");
     };
 
+    openPostModalCall = () => {
+        debugger;
+        this.props.openPostModalfns();
+    };
+
     render() {
         return (
             <div onClick={this.hideModal} id={"optionsModal"} className={this.state.isOpen ? 'optionsModal col l12 m12 s12' : 'optionsModal hide col l12 m12 s12'}>
@@ -21,10 +26,8 @@ class OptionsModal extends Component {
                         <ul className='option-ul'>
                             <li className="no-styles options-li">Report Inappropriate</li>
                             <li onClick={this.props.openUnfollowModal} className="no-styles options-li">Unfollow</li>
-                            <li className="no-styles options-li">Go To Post</li>
-                            <li className="no-styles options-li">Share</li>
+                            <li onClick={this.openPostModalCall} className="no-styles options-li">Go To Post</li>
                             <li className="no-styles options-li">Copy Link</li>
-                            <li className="no-styles options-li">Embed</li>
                             <li className="no-styles options-li">Cancel</li>
                         </ul>
                     </div>
